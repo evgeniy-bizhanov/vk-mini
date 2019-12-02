cefb489b8e4c529e43ecf0ad59fd57a0adebaf09
Бижанов Евгений <e.bizhanov@magdv.com>
2019-12-02 22:17:16 +0700

    Updates

---
+++ precache-manifest.73673ac7be2e5873e5b4dafb66545e7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78c24c07ddc8dcfa0799121ee75a548",
+    "url": "/vk-mini/index.html"
+  },
+  {
+    "revision": "dbbbab3095ebafe36970",
+    "url": "/vk-mini/static/css/2.0a8dee31.chunk.css"
+  },
+  {
+    "revision": "5e4d4bdad951267a6f65",
+    "url": "/vk-mini/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "dbbbab3095ebafe36970",
+    "url": "/vk-mini/static/js/2.0a0448c8.chunk.js"
+  },
+  {
+    "revision": "5e4d4bdad951267a6f65",
+    "url": "/vk-mini/static/js/main.cfd82cbc.chunk.js"
+  },
+  {
+    "revision": "b35ecd41cd1b316754e6",
+    "url": "/vk-mini/static/js/runtime-main.b3098f77.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-mini/static/media/persik.4e1ec840.png"
+  }
+]);
